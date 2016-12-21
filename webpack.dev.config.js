99f736b2502ca90fe3050a230f6fb62fa53932de
sopheak.peas <sopheak.peas@novacambodia.com>
2016-12-21 11:35:24 +0700

    Refactoring login and register modal

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,7 +5,7 @@ var PRODUCTION = process.env.NODE_ENV === 'production'; // injecting your Node.j
 var DEVELOPMENT = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  devtool: '#cheap-module-eval-source-map',
+  devtool: 'cheap-module-eval-source-map',
   entry: [
     'tether',
     'font-awesome-loader',
@@ -13,9 +13,9 @@ module.exports = {
     'jquery-hoverintent',
     'superfish',
     'sidr/dist/jquery.sidr',
-    './src/scripts/index.js',
     'webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:8080'
+    'webpack-dev-server/client?http://localhost:8080',
+    './src/scripts/index.js'
   ],
   output: {
     path: path.resolve(__dirname, "public/js/"), // the target directory for all output files
